15669cfe4228019639b062a8d14d31161edc581d
Youta Watanabe <yo-watanabe@coel-inc.jp>
2026-06-18 14:26:31 +0900

    C# Webアプリケーション ビルドの自動化

---
+++ CS_Actions_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS_Actions_WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
